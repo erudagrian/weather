008ca1fb0ce2902290e8f8a18685d40b7d6bec1c
Israel Baurel Vázquez Morales <galathisrael@yahoo.com.mx>
2018-01-12 02:17:30 -0600

    Weather elements

--- src/app/components/elements/card/card.component.ts
+++ src/app/components/elements/card/card.component.ts
@@ -63,15 +63,16 @@ import { YahooWeatherService } from '../../../services/yahoo-weather.service';
 export class CardComponent implements OnInit {
 
   @Input() location: City;
-  sufix: String;
+  sufix: string;
 
   cities: Condition;
-  hour: Number;
-  minute: Number;
-  weather: Number;
-  temp: String;
-  daytime: String;
-  description: String;
+  hour: number;
+  minute: number;
+  weather: number;
+  temp: string;
+  daytime: string;
+  description: string;
+  sky = 'blue';
 
   // Weather switches
   stormy = false;
@@ -83,7 +84,19 @@ export class CardComponent implements OnInit {
   windy = false;
   cloudy = false;
   clear = false;
-  semi = 6;
+  semi = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+  // Weather arrays
+  stormyArray = ['0', '1', '2', '3', '4', '37', '38', '39', '45', '47'];
+  tornadoArray = ['0', '1', '2'];
+  rainyArray = ['5', '6', '7', '8', '9', '10', '11', '12', '17', '18', '23', '35', '45', '46', '47'];
+  snowyArray = ['5', '6', '7', '8', '13', '14', '15', '16', '41', '42', '43', '46'];
+  dustyArray = ['19'];
+  foggyArray = ['20', '21', '22'];
+  windyArray = ['23', '24', '25', '41', '43'];
+  cloudyArray = ['25', '26', '27', '28', '29', '30', '44'];
+  clearArray = ['31', '32', '33', '34', '36'];
+  semiArray = ['29', '30', '37', '38', '39', '40', '42', '44', '47'];
 
   currentWeather: { type: 'thunder' };
 
@@ -93,7 +106,6 @@ export class CardComponent implements OnInit {
     this.sufix = 'weather';
     this.ywservice.getCurrentConditions(this.location.woeid).subscribe(city => {
       this.cities = city.query.results.channel.item.condition;
-      console.log(this.cities.code);
       this.weather = this.cities.code;
       this.temp = this.cities.temp + ' ºC';
       const date = new Date(this.cities.date.substring(0, this.cities.date.length - 4));
@@ -106,31 +118,41 @@ export class CardComponent implements OnInit {
       } else {
         this.daytime = 'afternoon';
       }
+      this.setWeather(this.cities.code.toString());
       this.description = this.cities.text;
     });
   }
 
-  setWeather(weatherCode: number) {
-    if (weatherCode in [0, 1, 2, 3, 4, 37, 38, 39, 45, 47]) {
+  setWeather(weatherCode: string) {
+    if (this.stormyArray.indexOf(weatherCode) > -1) {
       this.stormy = true;
-    } else if (weatherCode in [0, 1, 2]) {
+      this.sky = 'grey';
+    } else if (this.tornadoArray.indexOf(weatherCode) > -1) {
       this.tornado = true;
-    } else if (weatherCode in [5, 6, 7, 8, 9, 10, 11, 12, 17, 18, 23, 35, 45, 46, 47]) {
+      this.sky = 'grey';
+    } else if (this.rainyArray.indexOf(weatherCode) > -1) {
       this.rainy = true;
-    } else if (weatherCode in [5, 6, 7, 8, 13, 14, 15, 16, 41, 42, 43, 46]) {
+      this.sky = 'grey';
+    } else if (this.snowyArray.indexOf(weatherCode) > -1) {
       this.snowy = true;
-    } else if (weatherCode in [19]) {
+      this.sky = 'grey';
+    } else if (this.dustyArray.indexOf(weatherCode) > -1) {
       this.dusty = true;
-    } else if (weatherCode in [20, 21, 22]) {
+    } else if (this.foggyArray.indexOf(weatherCode) > -1) {
       this.foggy = true;
-    } else if (weatherCode in [23, 24, 25, 41, 43]) {
+      this.sky = 'grey';
+    } else if (this.windyArray.indexOf(weatherCode) > -1) {
       this.windy = true;
-    } else if (weatherCode in [25, 26, 27, 28, 29, 30, 44]) {
+    } else if (this.cloudyArray.indexOf(weatherCode) > -1) {
       this.cloudy = true;
-    } else if (weatherCode in [31, 32, 33, 34, 36]) {
+    } else if (this.clearArray.indexOf(weatherCode) > -1) {
       this.clear = true;
-    } else if (weatherCode in [29, 30, 37, 38, 39, 40, 42, 44, 47]) {
-      this.semi = 3;
+    } else if (this.semiArray.indexOf(weatherCode) > -1) {
+      this.semi = ['', 'a', 'b', 'c'];
     }
+    this.stormy = true;
+    this.cloudy = false;
+    this.sky = 'grey';
+    this.semi = ['', 'a', 'b', 'c'];
   }
 }
